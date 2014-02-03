9497b24214c55dcafaeb31ff8c86c75290eecbcf
aksiazek <alexander.ksiazek@gmail.com>
2014-02-03 12:57:59 +0100

    product rule

--- symbolic-derivation.lisp
+++ symbolic-derivation.lisp
@@ -1,11 +1,20 @@
 (defmacro print (&rest expr)
   `(quote ,expr))
 
-(defmacro derivative (var &rest expr)
-  `(quote ,expr))
+(defmacro derivative (var &rest expression)
+  `(let ((expr ',@expression))
+     (case (car expr)
+       ('* (list '+ (list '* (derivative (quote ,var) (cadr expr)) (cddr expr))
+		 (list '* (cadr expr) (derivative (quote ,var) (cddr expr))) ))
+       )))
+
 
 (defun is-named-function (name)
   (if (member name '(sin cos tg ctg sqrt exp ln log asin acos atg actg)) T nil))
 
-(defmacro named-function-derivative (name &rest args)
-  `(case (quote ,name) ('sin '(cos (quote ,args))) ))
+(defmacro named-function-derivative (name &rest arg)
+  `(case (quote ,name) 
+     ('sin '(cos ,@arg))
+     ('cos '(- (sin ,@arg)))
+     ('tg '(+ 1 (^ (tg ,@arg) 2)))
+     ))
